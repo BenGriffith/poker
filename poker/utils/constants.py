7f61adfc54280cf4d985a0c4f5f65c5200328549
Ben Griffith <bengriffith@outlook.com>
2023-03-27 02:09:43 +0000

    updated constants module and added Makefile

--- poker/utils/constants.py
+++ poker/utils/constants.py
@@ -1,3 +1,22 @@
+from enum import Enum
+
+
 SUITS = ["hearts", "diamonds", "spades", "clubs"]
 FACE_CARDS = "J Q K A".split()
 NUMBER_CARDS = list(range(2, 11))
+INCREMENT_LIMIT = 20
+SINGLE_CHIP = 1
+DOUBLE = 2
+
+
+class Chip(Enum):
+    WHITE = 1
+    RED = 5
+    BLUE = 10
+
+
+class Cash(Enum):
+    FIVE = 5
+    TEN = 10
+    FIFTEEN = 15
+    TWENTY = 20
