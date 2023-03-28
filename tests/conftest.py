96e3eeb5327d093597875f05f061b3a1d24ce209
Ben Griffith <bengriffith@outlook.com>
2023-03-28 02:19:20 +0000

    added Stack class for poker chips along with tests and custom exceptions

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,8 @@ import pytest
 
 from poker.utils.card import Card
 from poker.utils.deck import Deck
-from poker.utils.constants import SUITS, FACE_CARDS, NUMBER_CARDS
+from poker.utils.chip import Stack
+from poker.utils.constants import Chip, Cash
 
 
 @pytest.fixture
@@ -12,3 +13,15 @@ def card():
 @pytest.fixture
 def deck():
     return Deck()
+
+@pytest.fixture
+def stack():
+    return Stack()
+
+@pytest.fixture
+def chip():
+    return Chip
+
+@pytest.fixture
+def cash():
+    return Cash
