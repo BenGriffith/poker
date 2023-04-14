d7f59a25015b2feea8ae1c210db4673522022d61
Ben Griffith <bengriffith@outlook.com>
2023-04-14 04:02:26 +0000

    started implementing textual

--- poker/__main__.py
+++ poker/__main__.py
@@ -1,11 +1,33 @@
-from poker.utils.game import GameMessage, Game
+from poker.utils.game import GameMessage, Game, TestApp
 from poker.utils.player import Player, Dealer
-from poker.utils.constants import Decision
+
+from textual.app import App, ComposeResult
+from textual.widgets import Button, Header, Label
 
 
-game_message = GameMessage()
-dealer = Dealer()
-player = Player()
-game = Game(game_message, dealer, player)
-game.start()
-print("Thanks for joining! Hope to see you again soon!")
+class PokerApp(App):
+    CSS_PATH = "utils/css/main-menu.css"
+    TITLE = "Welcome to Texas hold'em!"
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Label("Would you like to play a game?", id="question")
+        yield Button("Yes", id="yes", variant="primary")
+        yield Button("No", id="no", variant="error")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        self.exit(event.button.id)
+
+app = PokerApp()
+reply = app.run()
+if reply == "yes":
+    test_app = TestApp()
+    test_app.run()
+
+
+# game_message = GameMessage()
+# dealer = Dealer()
+# player = Player()
+# game = Game(game_message, dealer, player)
+# game.start()
+# print("Thanks for joining! Hope to see you again soon!")
